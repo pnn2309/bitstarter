b8ec27b6bb10337bf2fa22cedaac6a5caeae534f
Phuc Nguyen <PhucNgocNguyen@gmail.com>
2013-08-16 17:35:49 +0000

    Last add index and web files

--- web.js
+++ web.js
@@ -1,11 +1,8 @@
 var fs = require('fs');
-
 var express = require('express');
-
 var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
-
 app.get('/', function(request, response) {
   var html = fs.readFileSync(htmlfile).toString();
   response.send(html);
